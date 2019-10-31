048d475b4a0b36b53c06e0a242835046282a7359
u049 <48441526+u049@users.noreply.github.com>
2019-10-31 09:57:10 +0800

    Update Header

--- Web/pages/header.php
+++ Web/pages/header.php
@@ -5,14 +5,21 @@
 <title><?php echo $page_type; ?></title>
 
 <!-- JS -->
+<!-- 3rd-Part Markdown Lib -->
 <script src="<?php echo URL_PATH; ?>/plugins/markdown/markdown.min.js"></script>
+<!-- 3rd-Part JQuery Lib, To Control Html Element-->
 <script src="<?php echo URL_PATH; ?>/plugins/jquery/jquery.min.js"></script>
+<!-- 3rd-Part JQuery Cookie Lib, To Modify and Read Cookie -->
 <script src="<?php echo URL_PATH; ?>/plugins/jquery/jquery.cookie.js"></script>
-
+<!-- 3rd-Part JQuery Scrollbars Lib, To hide Scrollbars  -->
 <script src="<?php echo URL_PATH; ?>/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<!-- 3rd-Part Bootstrap JS -->
 <script src="<?php echo URL_PATH; ?>/plugins/bootstrap/js/bootstrap.min.js"></script>
+<!-- 3rd-Part Bootstrap Theme JS -->
 <script src="<?php echo URL_PATH; ?>/plugins/bootstrap/js/theme.min.js"></script>
+<!-- 3rd-Part Toast Message -->
 <script src="<?php echo URL_PATH; ?>/plugins/toastr/toastr.min.js"></script>
+
 <script src="<?php echo URL_PATH; ?>/js/core.js"></script>
 <script src="<?php echo URL_PATH; ?>/js/user.js"></script>
 <script src="<?php echo URL_PATH; ?>/js/course.js"></script>
@@ -21,11 +28,15 @@
 <script type="text/javascript"> function getPath(){ return "<?php echo Folder_PATH; ?>"; } </script>
 
 <!-- CSS -->
+<!-- CSS for 3rd-Part JQuery Scrollbars -->
 <link rel="stylesheet" href="<?php echo URL_PATH; ?>/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+<!-- CSS for Fontawesome to make Icon beautiful -->
 <link rel="stylesheet" href="<?php echo URL_PATH; ?>/plugins/fontawesome-free/css/all.min.css">
+<!-- CSS For Bootstrap Theme -->
 <link href="<?php echo URL_PATH; ?>/plugins/bootstrap/css/theme.min.css" rel="stylesheet">
+<!-- CSS For Toast Message -->
 <link rel="stylesheet" href="<?php echo URL_PATH; ?>/plugins/toastr/toastr.min.css">
-
+<!-- 3-rd JS for HTML5 Support -->
 <!--[if lt IE 9]>
     <script src="https://cdn.jsdelivr.net/npm/html5shiv@3.7.3/dist/html5shiv.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/respond.js@1.4.2/dest/respond.min.js"></script>
